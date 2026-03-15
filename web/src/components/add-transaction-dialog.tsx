e864dd3ef00684f11ab74a6ee56e32196723a144
Luan de Souza Rosa <luan_souza_r@hotmail.com>
2026-03-14 23:08:10 -0300

    fix: revert to asChild prop for DialogTrigger to fix build

--- web/src/components/add-transaction-dialog.tsx
+++ web/src/components/add-transaction-dialog.tsx
@@ -110,11 +110,9 @@ export function AddTransactionDialog({ children }: { children?: React.ReactNode 
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      {children ? (
-        <DialogTrigger render={children} />
-      ) : (
-        <DialogTrigger render={triggerButton} />
-      )}
+      <DialogTrigger asChild>
+         {children ? children : triggerButton}
+      </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Adicionar Transação</DialogTitle>
